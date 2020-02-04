1dde16870cdd0981368231215a873d9e2a25b269
すー <aaaiiiuuu35@gmail.com>
2020-02-05 00:34:34 +0900

    8

--- api/src/getPage.ts
+++ api/src/getPage.ts
@@ -1,36 +1,41 @@
-const puppeteer = require('puppeteer');
+const { launch } = require('puppeteer');
 
-const xpathList: number[] = [8, 11, 14];
 const pageList: string[] = [
   'https://www.cit-s.com/mobile/tsuda_d/1-mon.html',
   'https://www.cit-s.com/mobile/tsuda_d/2-tue.html',
 ];
+const xpathList: number[] = [8, 11, 14];
 
 export const main = async () => {
-  let funcList: Function[] = [];
+  const funcList: any = [];
   pageList.map(async x => {
     xpathList.map(async y => {
       funcList.push(getContents(x, y));
     });
   });
+  funcList[0]();
   try {
-    await Promise.all(funcList).catch(e => console.log(e));
+    await Promise.all(funcList)
+      .then(results => {
+        console.log(results);
+      })
+      .catch(e => console.log(e));
   } catch (e) {
     throw e.message;
   } finally {
+    console.log('aaa');
   }
 };
 
 const getContents = (url: string, path: number) => {
-  const f = async () => {
+  return async () => {
+    console.log('ex');
     const xpath = `/html/body/text()[${path}]`;
-    const browser = await puppeteer
-      .launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      })
-      .catch(e => {
-        throw e;
-      });
+    const browser = await launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    }).catch((e: any) => {
+      throw e;
+    });
     const page = await browser.newPage();
     await page.goto(url);
     await page.waitForXPath(xpath);
@@ -38,7 +43,7 @@ const getContents = (url: string, path: number) => {
     const jsHandle = await elems[0].getProperty('textContent');
     const text = await jsHandle.jsonValue();
     browser.close();
+    console.log(text);
     return text;
   };
-  return f;
 };
