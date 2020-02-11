4481150a0a39b4ba4eedf1adbeab438d13e5bd82
すー <aaaiiiuuu35@gmail.com>
2020-02-11 22:24:56 +0900

    Update getPage.ts

--- api/src/getPage.ts
+++ api/src/getPage.ts
@@ -10,37 +10,44 @@ const pageList: [string, string][] = [
   ['金曜日', 'https://www.cit-s.com/mobile/tsuda_d/5-fri.html'],
   ['土曜日', 'https://www.cit-s.com/mobile/tsuda_d/6-sat.html'],
 ];
-const xpathList: number[] = [8, 11, 14, 15, 18, 21];
+const xpathList: number[] = [8, 11, 14, 15, 18, 21, 3];
+// const xpathList: number[] = [3];
 
 export const main = async () => {
   try {
+    const browser = await puppeteer
+      .launch({
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      })
+      .catch((e: any) => {
+        throw e;
+      });
     const week = await Promise.all([
       ...pageList.map(async x => {
         const [date] = await Promise.all([
           xpathList.map(async y => {
-            const browser = await puppeteer
-              .launch({
-                args: ['--no-sandbox', '--disable-setuid-sandbox'],
-              })
-              .catch((e: any) => {
-                throw e;
-              });
             const page = await browser.newPage();
             await page.goto(x[1]);
-            await page.waitForXPath(`/html/body/text()[${y}]`);
-            const elems = await page.$x(`/html/body/text()[${y}]`);
-            const jsHandle = await elems[0].getProperty('textContent');
-            const text = await jsHandle.jsonValue();
-            await console.log(text);
-            await browser.close();
-            return text.trim();
+            let text = '';
+            try {
+              await page.waitForXPath(`/html/body/text()[${y}]`, {
+                timeout: 3000,
+              });
+              const elems = await page.$x(`/html/body/text()[${y}]`);
+              const jsHandle = await elems[0].getProperty('textContent');
+              text = await jsHandle.jsonValue();
+            } catch (e) {
+              console.error(e);
+            }
+            // await console.log(text);
+            return text?.trim();
           }),
         ]);
         const dateMeal: schoolMeal = {
-          date: '',
+          date: await date[6],
           launchA: await date[0],
           launchB: await date[1],
-          launchC: [await date[2], date[3]],
+          launchC: [await date[2], await date[3]],
           dailySalad: await date[4],
           gourmetCurry: await date[5],
           createDate: new Date().toString(),
@@ -49,6 +56,7 @@ export const main = async () => {
         return dateMeal;
       }),
     ]);
+    await browser.close();
     console.log(week);
   } catch (e) {
     throw e.message;
