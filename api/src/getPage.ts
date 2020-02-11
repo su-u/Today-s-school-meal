74e23f63a257936421420d6cea3c8172a3e41dbe
すー <aaaiiiuuu35@gmail.com>
2020-02-11 16:56:47 +0900

    メニュー取得

--- api/src/getPage.ts
+++ api/src/getPage.ts
@@ -1,49 +1,58 @@
-const { launch } = require('puppeteer');
+const puppeteer = require('puppeteer');
+import { schoolMeal } from './schoolMeal';
+process.setMaxListeners(50);
 
-const pageList: string[] = [
-  'https://www.cit-s.com/mobile/tsuda_d/1-mon.html',
-  'https://www.cit-s.com/mobile/tsuda_d/2-tue.html',
+const pageList: [string, string][] = [
+  ['月曜日', 'https://www.cit-s.com/mobile/tsuda_d/1-mon.html'],
+  ['火曜日', 'https://www.cit-s.com/mobile/tsuda_d/2-tue.html'],
+  ['水曜日', 'https://www.cit-s.com/mobile/tsuda_d/3-wed.html'],
+  ['木曜日', 'https://www.cit-s.com/mobile/tsuda_d/4-thu.html'],
+  ['金曜日', 'https://www.cit-s.com/mobile/tsuda_d/5-fri.html'],
+  ['土曜日', 'https://www.cit-s.com/mobile/tsuda_d/6-sat.html'],
 ];
 const xpathList: number[] = [8, 11, 14];
 
 export const main = async () => {
-  const funcList: any = [];
-  pageList.map(async x => {
-    xpathList.map(async y => {
-      funcList.push(getContents(x, y));
-    });
-  });
-  funcList[0]();
   try {
-    await Promise.all(funcList)
-      .then(results => {
-        console.log(results);
-      })
-      .catch(e => console.log(e));
+    const week = await Promise.all([
+      ...pageList.map(async x => {
+        const [date] = await Promise.all([
+          xpathList.map(async y => {
+            const browser = await puppeteer
+              .launch({
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+              })
+              .catch((e: any) => {
+                throw e;
+              });
+            const page = await browser.newPage();
+            await page.goto(x[1]);
+            await page.waitForXPath(`/html/body/text()[${y}]`);
+            const elems = await page.$x(`/html/body/text()[${y}]`);
+            const jsHandle = await elems[0].getProperty('textContent');
+            const text = await jsHandle.jsonValue();
+            // await console.log(text);
+            await browser.close();
+            return text.trim();
+          }),
+        ]);
+
+        const dateMeal: schoolMeal = {
+          date: '',
+          launchA: await date[0],
+          launchB: await date[1],
+          launchC: [await date[2], date[3]],
+          dailySalad: await date[4],
+          gourmetCurry: await date[5],
+          createDate: new Date().toString(),
+        };
+        // console.log({ dateMeal });
+        return dateMeal;
+      }),
+    ]);
+    console.log(week);
   } catch (e) {
     throw e.message;
   } finally {
-    console.log('aaa');
   }
-};
-
-const getContents = (url: string, path: number) => {
-  return async () => {
-    console.log('ex');
-    const xpath = `/html/body/text()[${path}]`;
-    const browser = await launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    }).catch((e: any) => {
-      throw e;
-    });
-    const page = await browser.newPage();
-    await page.goto(url);
-    await page.waitForXPath(xpath);
-    const elems = await page.$x(xpath);
-    const jsHandle = await elems[0].getProperty('textContent');
-    const text = await jsHandle.jsonValue();
-    browser.close();
-    console.log(text);
-    return text;
-  };
 };
